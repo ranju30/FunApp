89b7f9d4cd760dd98d718de29dfc92417cd0586b
Ranju <ranjukr@thoughtworks.com>
2017-01-14 16:47:27 +0530

    @ranjukr: added test for zero

---
+++ number_to_word/test/zeroTest.js
@@ -0,0 +1,25 @@
+var assert = require('chai').assert;
+var Zero = require('../lib/zero');
+
+describe('zero', function() {
+    describe('isZero', function() {
+        it('should return true if total number is zero', function() {
+            var zero = new Zero();
+            var ans = zero.isZero(00);
+            assert.isTrue(ans);
+        });
+        it('should return false if total number is non zero', function() {
+            var zero = new Zero();
+            var ans = zero.isZero(001);
+            assert.isFalse(ans);
+        });
+    });
+    describe('represent', function() {
+        it('should return zero', function() {
+            var zero = new Zero();
+            var ans = zero.represent();
+            assert.equal(ans,'zero');
+        });
+    });
+
+});
