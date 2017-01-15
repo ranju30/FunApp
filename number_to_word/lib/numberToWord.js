9c4094086663b7e40d84dce71610494f2287c071
Ranju <ranjukr@thoughtworks.com>
2017-01-15 19:58:59 +0530

    @ranjukr: added represent upto 999.

--- number_to_word/lib/numberToWord.js
+++ number_to_word/lib/numberToWord.js
@@ -1,10 +1,20 @@
 var Zero = require('./Zero');
-var zero = new Zero();
+var OneToNine = require('./OneToNine');
+var TenToNinetyNine = require('./TenToNinetyNine');
+var HundredToNineNinetyNine = require('./HundredToNineNinetyNine');
 
+var zero = new Zero();
+var oneToNine = new OneToNine();
+var tenToNinetyNine = new TenToNinetyNine();
+var hundredToNineNinetyNine = new HundredToNineNinetyNine();
+
+var representer = [,oneToNine,tenToNinetyNine,hundredToNineNinetyNine]
 
 var ToWord = function(){
   this.represent = function(number){
     if(zero.isZero(number)) return zero.represent();
+    length = number.toString().length;
+    return representer[length].represent(number);
   }
 }
 
--- number_to_word/test/numberToWordTest.js
+++ number_to_word/test/numberToWordTest.js
@@ -12,6 +12,38 @@ describe('represent',function(){
 		var inWord = toWord.represent(000);
 		assert.equal(inWord,'zero');
 	});
+	it('should represent number for one',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(1);
+		assert.equal(inWord,'one');
+	});
+
+	it('should represent for number nine',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(9);
+		assert.equal(inWord,'nine');
+	});
+	it('should represent for number from 10 to 20',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(15);
+		assert.equal(inWord,'fifteen');
+	});
+
+	it('should represent for number above 20',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(33);
+		assert.equal(inWord,'thirty three');
+	});
+
+	it('should represent for number above 100',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(330);
+		assert.equal(inWord,'three hundred thirty');
+	});
+
+	it('should represent for number above 100',function(){
+		var toWord = new ToWord();
+		var inWord = toWord.represent(900);
+		assert.equal(inWord,'nine hundred');
+	});
 });
-
-
