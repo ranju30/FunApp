a4dfe8ebd2beebed9b06f6bb9f3def33d741f9bd
Ranju <ranjukr@thoughtworks.com>
2017-01-16 00:17:31 +0530

    @ranjukr: added thousand to four nine representor with test.

---
+++ number_to_word/lib/thousandToFourNine.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+
+var Zero = require('../lib/zero');
+var OneToNine = require('./oneToNine');
+var HundredToNineNinetyNine = require('./hundredToNineNinetyNine');
+
+var zero = new Zero();
+var oneToNine = new OneToNine();
+var hundredToNineNinetyNine = new HundredToNineNinetyNine();
+
+var ThousandToFourNine = function() {
+    this.represent = function(number) {
+        if (zero.isZero(number)) return '';
+        return _.trim(oneToNine.represent(Math.trunc(number / 1000)) + " thousand " + hundredToNineNinetyNine.represent(number % 1000));
+    }
+}
+
+module.exports = ThousandToFourNine;
---
+++ number_to_word/test/thousandToFourNineTest.js
@@ -0,0 +1,35 @@
+var assert = require('chai').assert;
+var ThousandToFourNine = require('../lib/thousandToFourNine');
+
+describe('thousandToFourNine', function() {
+    describe('represent', function() {
+        var thousandToFourNine;
+        beforeEach(function() {
+            thousandToFourNine = new ThousandToFourNine();
+        });
+        it('should return the word for 1000', function() {
+            var ans = thousandToFourNine.represent(1000);
+            assert.equal(ans, 'one thousand');
+        });
+        it('should return the word for 1332', function() {
+            var ans = thousandToFourNine.represent(1332);
+            assert.equal(ans, 'one thousand three hundred thirty two');
+        });
+        it('should return the word for 2020', function() {
+            var ans = thousandToFourNine.represent(2020);
+            assert.equal(ans, 'two thousand twenty');
+        });
+        it('should return the word for 5647', function() {
+            var ans = thousandToFourNine.represent(5647);
+            assert.equal(ans, 'five thousand six hundred fourty seven');
+        });
+        it('should return the word for 7137', function() {
+            var ans = thousandToFourNine.represent(7137);
+            assert.equal(ans, 'seven thousand one hundred thirty seven');
+        });
+        it('should return the word for 9989', function() {
+            var ans = thousandToFourNine.represent(9989);
+            assert.equal(ans, 'nine thousand nine hundred eighty nine');
+        });
+    });
+});
