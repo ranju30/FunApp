dba63b76912f004d133ae9145a7db31b12b0403d
Ranju <ranjukr@thoughtworks.com>
2017-01-15 23:15:40 +0530

    @ranjukr: added tenToNinetyNine representor with tests.

---
+++ number_to_word/test/tenToNinetyNineTest.js
@@ -0,0 +1,35 @@
+var assert = require('chai').assert;
+var TenToNinetyNine = require('../lib/tenToNinetyNine');
+
+describe('tenToNinetyNine', function() {
+    describe('represent', function() {
+        var tenToNinetyNine;
+        beforeEach(function() {
+            tenToNinetyNine = new TenToNinetyNine();
+        });
+        it('should return the word for 10', function() {
+            var ans = tenToNinetyNine.represent(10);
+            assert.equal(ans, 'ten');
+        });
+        it('should return the word for 13', function() {
+            var ans = tenToNinetyNine.represent(13);
+            assert.equal(ans, 'thirteen');
+        });
+        it('should return the word for 20', function() {
+            var ans = tenToNinetyNine.represent(20);
+            assert.equal(ans, 'twenty');
+        });
+        it('should return the word for 56', function() {
+            var ans = tenToNinetyNine.represent(56);
+            assert.equal(ans, 'fifty six');
+        });
+        it('should return the word for 71', function() {
+            var ans = tenToNinetyNine.represent(71);
+            assert.equal(ans, 'seventy one');
+        });
+        it('should return the word for 99', function() {
+            var ans = tenToNinetyNine.represent(99);
+            assert.equal(ans, 'ninety nine');
+        });
+    });
+});
